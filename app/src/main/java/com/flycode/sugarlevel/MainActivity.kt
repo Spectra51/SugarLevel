fd07462e17cff92fa321fc8dedfb98adf378a9a8
Spectra <monctrik@yandex.ru>
2022-12-27 18:26:55 +0300

    Revert "fourth commit"
    This reverts commit 06fec8b0dff47b61e1736787c7f5c9a9279edc00.

--- app/src/main/java/com/flycode/sugarlevel/MainActivity.kt
+++ app/src/main/java/com/flycode/sugarlevel/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-ваыаыаываыва
+
         println("THIS IS Secoond commit")
+        today is a good day
     }
 }
