7502213d112f63ffbb5ee8060ab86adcb5b05e6e
Spectra <monctrik@yandex.ru>
2022-12-27 20:39:46 +0300

    final merge change 2

--- app/src/main/java/com/flycode/sugarlevel/MainActivity.kt
+++ app/src/main/java/com/flycode/sugarlevel/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("THIS IS Secoond commit")
 
-        print("Changeeeeeeeee")
+        print("Another change")
 
         print("For merge")
     }
